3b462248215f9f6d03c72eb69ce9d31e031a6317
Fanta Kébé <Kebef@outlook.com>
2025-11-02 18:58:04 -0500

    A1

--- app/Labs/page.tsx
+++ app/Labs/page.tsx
@@ -3,6 +3,7 @@ export default function Labs() {
  return (
    <div id="wd-labs">
      <h1>Labs</h1>
+     <h2>Fanta Kebe - CS4550</h2>
      <ul>
        <li>
          <Link href="/Labs/Lab1" id="wd-lab1-link">
@@ -19,6 +20,4 @@ export default function Labs() {
      </ul>
    </div>
 );}
-
-
 
--- app/page.tsx
+++
@@ -1,6 +0,0 @@
-import { redirect } from "next/navigation";
-
-export default function Home() {
-  redirect("/Kambaz");
-  return null;
-}
