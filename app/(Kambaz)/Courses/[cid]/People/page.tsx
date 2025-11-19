39608fb96881fdc7d7b4fc9320e720cede05c915
Fanta Kébé <Kebef@outlook.com>
2025-11-19 02:11:05 -0500

    Finish Lab 2: People page and tables

---
+++ app/(Kambaz)/Courses/[cid]/People/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+import PeopleTable from "./Table/page";
+
+export default function People() {
+  return (
+    <div id="wd-people-screen" className="p-3">
+      <PeopleTable />
+    </div>
+  );
+}
